81df89b67e7fe0dbe9241340698b010bf3360038
Brian Malkan <brmalkan@gmail.com>
2026-02-12 01:12:37 -0500

    basic callback + obj id's added

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,6 +1,8 @@
-from dash import Dash
+from dash import Dash, Input, Output, State
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
 
+from data_loader import load_starset_data, load_orbits
 from layout import app_layout
 
 
@@ -16,4 +18,55 @@ class Dashboard:
         self.app.layout = app_layout()
 
     def start(self) -> None:
+
+        @self.app.callback(
+            Output("star_map_2d", "figure"),
+            Output("star_map_3d", "figure"),
+            Input("refresh_button", "n_clicks"),
+            State("data_filepath", "value"),
+            State("orbit_filepath", "value"),
+            State("map_range", "value"),
+            State("map_center_x", "value"),
+            State("map_center_y", "value"),
+            State("map_radio_options", "value")
+        )
+        def update_starmap(
+            _refresh_button: int,
+            data_filepath: str,
+            orbit_filepath: str,
+            range: float,
+            center_x: float,
+            center_y: float,
+            radio_options: list[bool],
+        ) -> None:
+            if not data_filepath:
+                return go.Figure() # TODO Add Error
+
+            star_data = load_starset_data(data_filepath)
+            show_name = True
+
+            fig_2d = go.Scatter(
+                x=star_data["x"],
+                y=star_data["y"],
+                xaxis={
+                    "range": [center_x + range, center_x - range]
+                },
+                yaxis={
+                    "range": [center_y + range, center_y - range]
+                },
+                mode="markers+text" if show_name else "text",
+                text=star_data["name"] if show_name else None,
+                marker={
+                    "size": 1,
+                    "color": "cyan",
+                    "opacity": 0.7
+                }
+            )
+            fig_3d = go.Scatter3d()
+
+            if orbit_filepath:
+                orbit_data = load_orbits(orbit_filepath)
+
+            return fig_2d, None
+
         self.app.run(port=8050)
---
+++ src/data_loader.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+from gcwork import starset
+from gcwork import orbits
+
+def load_starset_data(data_filepath: str) -> dict[str, list[float]]:
+    """
+    Load star position data from alignment files
+    """
+
+    s = starset.StarSet(data_filepath)
+
+    name = np.array(s.getArray('name')).tolist()
+    x = np.array(s.getArray('x')).tolist()
+    y = np.array(s.getArray('y')).tolist()
+    xe = np.array(s.getArray('xerr')).tolist()
+    ye = np.array(s.getArray('yerr')).tolist()
+    vx = np.array(s.getArray('vx')).tolist()
+    vy = np.array(s.getArray('vy')).tolist()
+    vxe = np.array(s.getArray('vxerr')).tolist()
+    vye = np.array(s.getArray('vyerr')).tolist()
+    mag = np.array(s.getArray('mag')* 1.0).tolist()
+    nEpochs = np.array(s.getArray('velCnt')).tolist()
+
+    data = {
+        'name': name,
+        'x': x,
+        'y': y,
+        'xe': xe,
+        'ye': ye,
+        'vx': vx,
+        'vy': vy,
+        'vxe': vxe,
+        'vye': vye,
+        'mag': mag,
+        'nEpochs': nEpochs
+    }
+
+    return data
+
+def find_star_coordinates(data, star_name):
+    """Find star coordinates by name"""
+    try:
+        idx = data['name'].index(star_name)
+        return {
+            'found': True,
+            'x': data['x'][idx],
+            'y': data['y'][idx]
+        }
+    except ValueError:
+        return {'found': False}
+
+def load_orbits(
+    orbits_file: str, tStart=1994., tEnd=2020., dt=0.01
+) -> tuple[list[any], list[any]]:
+    """
+    Load orbit data from file
+    """
+    if not orbits_file:
+        return None, None, False
+
+    t = np.linspace(tStart, tEnd, int(np.ceil((tEnd - tStart) / dt)))
+
+    if orbits_file is None:
+        tab = np.genfromtxt(orbits_file, dtype=str)
+    else:
+        tab = np.genfromtxt(orbits_file, dtype=str)
+
+    res = []
+    names = []
+    for star in tab:
+        orb = orbits.Orbit()
+        orb.p = float(star[1])
+        orb.t0 = float(star[3])
+        orb.e = float(star[4])
+        orb.i = float(star[5])
+        orb.o = float(star[6])
+        orb.w = float(star[7])
+
+        (x, v, a) = orb.kep2xyz(epochs=t, mass=4.e6, dist=8000.)  # possible to change mass and R0 here if needed
+        names.append(star[0])
+        res.append(x[:, 0:2].tolist())  # Convert numpy array to list
+
+    return res, names
